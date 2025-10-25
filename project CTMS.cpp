7ac5b6e0cbdbff6f5fd4529de12d83fc80f67102
TIRTHA DAS <tirthake@gmail.com>
2025-10-25 20:29:52 +0600

    Refactor CTMS version week 0.0.1
    just login page and  layout

--- project CTMS.cpp
+++ project CTMS.cpp
@@ -1,109 +1,140 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Structure to store train information
-struct Train
+class TrainManagementSystem  //parent classs for this project
  {
-    int trainNumber;
-    string trainName;
-    string source;
-    string destination;
-    int totalSeats;
-    int availableSeats;
-    double ticketPrice;
-    string departureTime;
-};
-
-// Structure to store ticket information
-struct Ticket
- {
-    int ticketId;
-    int trainNumber;
-    string passengerName;
-    int numberOfSeats;
-    double totalAmount;
-    string bookingDate;
-    bool isCancelled;
-};
-
-//Global vectors to store data
-vector<Train> trains;  //to store stains
-vector<Ticket> tickets;// to store tickets
-int ticketCounter = 1000;//initial 
+private:
+    const string ADMIN_PASSWORD = "tfjr100";
 
 
-//declare cnst Admin password
-const string ADMIN_PASSWORD = "admin123";
+    //use clearscreen to clear terminal
+    void clearScreen() {
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+    }
 
-//  forword Function declarations of our all function that we work on 
-void clearScreen();
-void pauseScreen();
-void mainMenu();
-void adminLogin();
-void adminMenu();
-void userGuestMode();
- void initializeSampleData();
-// void addTrain();
-// void deleteTrain();
-// void manageTrainFunction();
-// void viewAllTrains();
-// void searchTrainByNumber();
-// void buyTicket();
-// void cancelTicket();
-// void viewTicket();
-// void showTrainSchedule();
+    //use pause screen to wait for user cinput
+    void pauseScreen() {
+        cout << "\nPress Enter to continue...";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //ignore previous 
+        cin.get();
+    }
 
-//--------------------------------------------------------------
+public:
+    // Main Menu
+    void mainMenu()
+     {
+        while (true)
+         {
+            clearScreen();
+            cout << "\n========================================\n";
+            cout << "   TRAIN MANAGEMENT SYSTEM\n";
+            cout << "========================================\n";
+            cout << "1. Admin Login\n";
+            cout << "2. User (Guest Mode)\n";
+            cout << "3. Exit\n";
+            cout << "========================================\n";
+            cout << "Enter your choice: ";
+            
+            int choice;
+            cin >> choice;
+            
+            switch (choice)
+             {
+                case 1:
+                    adminLogin(); //call adminlogin method to perform
+                    break;
+                case 2:
+                    userGuestMode(); //call usermode method
+                    break;
+                case 3:
+                    cout << "\nThank you for using CTMS!\n";
+                    return;
+                default:
+                    cout << "\nInvalid choice! Please try again.\n";
+                    pauseScreen();
+            }
+        }
+    }
 
-// Main function to keep it simple , we use a function called main menu 
-int main() 
-{
-    initializeSampleData(); // Add some sample trains for initial work using vector pushback
-    mainMenu();
-    return 0;
-}
-///-------------------------------------------------------------
-// Clear screen function
-/*we use clear screen to clear our terminal everytimr we run any function , thats gives us more visivility in our terminal*/
-void clearScreen() // a  function with no perameter
- {
-    #ifdef _WIN32 //check it run on windows
-        system("cls"); //clear using system 
-    #else
-        system("clear"); ///otherwise use other operating system clear
-    #endif
-}
-
-// Pause screen function
-/* pause the screen untill user give any key , also it ignores any previous numeric value */
-void pauseScreen()
- {
-    cout << "\nPress Enter to continue...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');//ignore
-    cin.get();//get instruction from usser using get
-}
-
-// Initialize sample train data for work(number,name,source,destination,totoal s , av s ,price, time)
-void initializeSampleData()
- {
-    trains.push_back({101, "Dhaka Express", "Dhaka", "Chittagong", 200, 200, 500.0, "08:00 AM"});
-    trains.push_back({102, "Sylhet Mail", "Dhaka", "Sylhet", 150, 150, 400.0, "10:30 AM"});
-    trains.push_back({103, "Rajshahi Express", "Dhaka", "Rajshahi", 180, 180, 450.0, "02:00 PM"});
-    trains.push_back({104, "Khulna Special", "Dhaka", "Khulna", 160, 160, 420.0, "06:00 PM"});
-}
-
-// Main Menu
-void mainMenu()
- {
-    while (true)
+    // Admin Login
+    void adminLogin()
      {
         clearScreen();
         cout << "\n========================================\n";
-        cout << "   TRAIN MANAGEMENT SYSTEM\n";
+        cout << "        ADMIN LOGIN\n";
         cout << "========================================\n";
-        cout << "1. Admin Login (By Pass)\n";
-        cout << "2. User (Guest Mode)\n";
-        cout << "3. Exit\n";
+        
+        string password;
+        cout << "Enter Admin Password: ";
+        cin >> password;
+        
+        if (password == ADMIN_PASSWORD)//check password condition
+         {
+            cout << "\nLogin Successful!\n";
+            pauseScreen();
+            adminMenu(); //call admin menu for further work
+        } 
+        else
+         {
+            cout << "\nIncorrect Password! Access Denied.\n";
+            pauseScreen();
+        }
+    }
+
+    // Admin Menu
+    void adminMenu()
+     {
+        while (true) {
+            clearScreen();
+            cout << "\n========================================\n";
+            cout << "        ADMIN PANEL\n";
+            cout << "========================================\n";
+            //write method of admin menus
+            cout << "1. Manage Train Function\n";
+            cout << "2. View All Trains\n";
+            cout << "3. Show Train Schedule (Two D1-D2)\n";
+            cout << "4. Back to Main Menu\n";
+            cout << "========================================\n";
+            cout << "Enter your choice: ";
+            
+            int choice;
+            cin >> choice;
+            
+            switch (choice) //getting input via switch
+            {
+                case 1:
+                    manageTrainFunction(); 
+                    break;
+                case 2:
+                    viewAllTrains();
+                    break;
+                case 3:
+                    showTrainSchedule();
+                    break;
+                case 4:
+                    return;
+                default:
+                    cout << "\nInvalid choice! Please try again.\n";
+                    pauseScreen();
+            }
+        }
+    }
+
+    // Manage Train Function details
+    void manageTrainFunction()
+    
+    {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "    MANAGE TRAIN FUNCTION\n";
+        cout << "========================================\n";
+        cout << "1. Add Train\n";
+        cout << "2. Delete Train\n";
+        cout << "3. Back to Admin Menu\n";
         cout << "========================================\n";
         cout << "Enter your choice: ";
         
@@ -113,61 +144,134 @@ void mainMenu()
         switch (choice)
          {
             case 1:
-                adminLogin(); //call adminlogin
+                addTrain();
                 break;
             case 2:
-                userGuestMode();
+                deleteTrain();
                 break;
             case 3:
-                cout << "\n Thank you for using CTMS!\n";
                 return;
             default:
-                cout << "\n Invalid choice! Please try again.\n";
+                cout << "\nInvalid choice! Please try again.\n";
                 pauseScreen();
+                manageTrainFunction();
         }
     }
-    
-}
 
-//adminlogin
-
-void adminLogin()
- {
-    clearScreen();
-    cout << "\n========================================\n";
-    cout << "        ADMIN LOGIN\n";
-    cout << "========================================\n";
-    
-    string password;
-    cout << "Enter Admin Password: ";
-    cin >> password;
-    
-    if (password == ADMIN_PASSWORD) //check condition
+    // User Guest Mode
+    void userGuestMode()
      {
-        cout << "\nLogin Successful!\n";
-        pauseScreen();
-       // adminMenu();  add next
-    } else {
-        cout << "\nIncorrect Password! Access Denied.\n";
+        while (true) {
+            clearScreen();
+            cout << "\n========================================\n";
+            cout << "        USER (GUEST MODE)\n";
+            cout << "========================================\n";
+            cout << "1. View Ticket\n";
+            cout << "2. Buy Ticket\n";
+            cout << "3. Cancel Ticket\n";
+            cout << "4. Back to Main Menu\n";
+            cout << "========================================\n";
+            cout << "Enter your choice: ";
+            
+            int choice;
+            cin >> choice;
+            
+            switch (choice)
+             {
+                case 1:
+                    viewTicket();
+                    break;
+                case 2:
+                    buyTicket();
+                    break;
+                case 3:
+                    cancelTicket();
+                    break;
+                case 4:
+                    return;
+                default:
+                    cout << "\nInvalid choice! Please try again.\n";
+                    pauseScreen();
+            }
+        }
+    }
+
+    // ========================================
+    // this functions we implimented in future
+    // ========================================
+    
+    void addTrain()
+     {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          ADD NEW TRAIN\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
         pauseScreen();
     }
-}
-// userguestmode
-void userGuestMode() {
-    //need inprovement
-        cout << "\n========================================\n";
-        cout << "        USER (GUEST MODE)\n";
-        cout << "========================================\n";
-        cout << "1. View Ticket\n";
-        cout << "2. Buy Ticket\n";
-        cout << "3. Cancel Ticket\n";
-        cout << "4. Back to Main Menu\n";
-        cout << "========================================\n";
-        cout << "Enter your choice: ";
-        
-        int choice;
-        cin >> choice;
-        
-   
-}
 
+    void deleteTrain() 
+    {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          DELETE TRAIN\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+
+    void viewAllTrains()
+     {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          ALL TRAINS\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+
+    void showTrainSchedule() {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "      TRAIN SCHEDULE (D1-D2)\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+
+    void viewTicket()
+     {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          VIEW TICKET\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+
+    void buyTicket() {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          BUY TICKET\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+
+    void cancelTicket() {
+        clearScreen();
+        cout << "\n========================================\n";
+        cout << "          CANCEL TICKET\n";
+        cout << "========================================\n";
+        cout << "\nFunction to be implemented in future code\n";
+        pauseScreen();
+    }
+};
+
+// Main function
+int main()
+ {
+    TrainManagementSystem tms; //tms is an object of main class
+    tms.mainMenu(); //calling main by tms object reference
+    return 0;
+}
